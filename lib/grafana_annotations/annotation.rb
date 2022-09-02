c9e6e61c7f543b8ddb268e7443610063bcef5281
Konstantin Ivanov <konstantin.ivanov@sbermarket.ru>
2022-09-02 14:53:34 +0300

    fix rubocop

--- lib/grafana_annotations/annotation.rb
+++ lib/grafana_annotations/annotation.rb
@@ -4,7 +4,7 @@ module GrafanaAnnotations
   class Annotation < Dry::Struct
     attribute? :dashboardUID, Types::Strict::String.optional
     attribute? :panelId, Types::Strict::String.optional
-    attribute :time, Types::Strict::Integer.default { GrafanaAnnotations::Utils::Time.now_ms } # rubocop:disable Lint/AmbiguousBlockAssociation
+    attribute(:time, Types::Strict::Integer.default { GrafanaAnnotations::Utils::Time.now_ms })
     attribute? :timeEnd, Types::Strict::Integer
     attribute :tags, Types::Strict::Array.of(Types::Coercible::String)
     attribute :text, Types::Strict::String
